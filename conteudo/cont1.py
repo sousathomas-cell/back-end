c9b2622754e2e1c3acd030a1d6562b2198917a61
sousathomas-cell <sousa.thomas@escola.pr.gov.br>
2026-03-09 08:39:31 -0300

    Create cont1.py

---
+++ conteudo/cont1.py
@@ -0,0 +1,9 @@
+Int para números inteiros. Ex: 7, 0, -4
+
+Float para números reais ou com vírgula. Ex: 0.45, 7.6
+
+Bool para valores lógicos de verdadeiro ou falso. Ex: True ou False (sempre usar com letra maiúscula)
+
+Str para strings que são palavras, caracteres ou textos. Ex: 'ola', '7.5'
+
+Para saber o tipo de uma variável pode-se usar o comando print(type(variavel))
