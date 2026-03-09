2a9b60d769496114b7ba8a240d91e629e5d12b4c
sousathomas-cell <sousa.thomas@escola.pr.gov.br>
2026-03-09 08:44:11 -0300

    Create ex004.py

---
+++ exercicios/ex004.py
@@ -0,0 +1,7 @@
+n1 = int(input("digite um numero"))
+n2 = int(input("digite um numero"))
+
+soma= n1 + n2
+
+#print("a soma entre {} '.float(num1),' é {}' .format(num2), 'é {}'.format(soma))
+print('a soma entre {} e {} é {}'.format(n1,n2,soma))
