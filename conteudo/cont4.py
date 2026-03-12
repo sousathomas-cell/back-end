036049dad9653f26f919fd83c561156c28844cf0
sousathomas-cell <sousa.thomas@escola.pr.gov.br>
2026-03-12 09:04:29 -0300

    Create cont4.py

---
+++ conteudo/cont4.py
@@ -0,0 +1,7 @@
+#o + é utiliza como operador de adição 
+#o - utiliza como sinal de subtração 
+#o * utilizado para fazer a multiplicação 
+#o / é utilizada para fazer a divisão 
+#o ** é utilizado para fazer exponenciação
+#o // é utilizado para fazer divisão inteira 
+#o % é utilizada para o resto da divisão
