13ce597a2f5ae51d30c2ef16cfacceace3967cc1
sousathomas-cell <sousa.thomas@escola.pr.gov.br>
2026-03-16 10:57:29 -0300

    Create cont5.py

---
+++ conteudo/cont5.py
@@ -0,0 +1,22 @@
+#definição:é a ordem na qual os operadores serão executados 
+#1 exxpressões dentro de parenteses
+#2 em seguida vem as potencições 
+#3 em terceiro lugar vem a multipicação,a divisão normal,a
+#divisao inteira e o resto da divisão. no caso de ter mais de um deles 
+#você resolve quem aparercer primeiro 
+#4 e por ultimo a soma e a subtração
+
+#exemplo 
+#5+3*2==
+#3*5+4**2=
+#3*(5+4)**2=
+#O python é utilizado em calculos científicos é o tamanho da
+#memoria ou seja conseguimos chegar a números extremente grandes
+
+#outros operadores 
+#para exponenciação podemos usar o pow(x.y) onde o número x é elevado 
+#ao número y
+#para raiz quadrada podemos usar o**(1/2).elevando o número a 1/2 nos
+#obtemos a raiz quadrada,para raiz cúbica**(1/3).e assim por diante.
+#podemos usar os operadores em strings
+#ex '='*5 vai ficar'====='
