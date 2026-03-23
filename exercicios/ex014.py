1d11e1bec60df69e3a842114a3e407e38f377bd2
sousathomas-cell <sousa.thomas@escola.pr.gov.br>
2026-03-23 10:59:57 -0300

    Update ex014.py

--- exercicios/ex014.py
+++ exercicios/ex014.py
@@ -1,1 +1,4 @@
 
+salario_atual = float(input("Digite o salário atual do funcionário: R$ "))
+novo_salario = salario_atual * 1.15
+print(f"O novo salário com 15% de aumento é: R$ {novo_salario:.2f}")
