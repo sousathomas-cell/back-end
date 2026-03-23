cd65d629d7217dbf5f18221eebf72135afb16adb
sousathomas-cell <sousa.thomas@escola.pr.gov.br>
2026-03-23 10:48:59 -0300

    Create ex013.py

---
+++ exercicios/ex013.py
@@ -0,0 +1,3 @@
+preço = float(input('qual é o preço do produto?R$'))
+desconto = preço - (preço * 5/100)
+print('o produto que custava{:.2f}, na promoção com desconto de 5% vai custar r${:.2f}'.format(preço,desconto)
