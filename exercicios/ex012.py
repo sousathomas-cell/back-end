0188abc445c868497d0b71168148db9c4ee0284d
sousathomas-cell <sousa.thomas@escola.pr.gov.br>
2026-03-23 10:41:24 -0300

    Update ex012.py

--- exercicios/ex012.py
+++ exercicios/ex012.py
@@ -1,1 +1,6 @@
-
+largura = float(input('largura da parede'))
+altura = float(input('altura da parede'))
+área = largura*altura
+print('sua parede tem a dimensão de {} x {} e a sua área é de {}m quadradros;'.format(largura,altura,
+largura*altura))
+print('para pintar essa parede, vocẽ precisa de{}L de tinta'.format(área/2))
