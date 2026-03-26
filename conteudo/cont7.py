732272af6f80e17383bd3c8456d06dfc72f41a78
sousathomas-cell <sousa.thomas@escola.pr.gov.br>
2026-03-26 08:25:27 -0300

    Create cont7.py

---
+++ conteudo/cont7.py
@@ -0,0 +1,7 @@
+dias = int(input('quantos dias alugados?'))
+km = float(input('quantos km rodados?'))
+diarias = dias*60
+percurso = km * 0.15
+custo = + diaria + percurso
+
+print('o total a pagar é de r${:.2f}'.format(custo))
