a571764c05c2000fed3bfdb602614a08744e7d14
sousathomas-cell <sousa.thomas@escola.pr.gov.br>
2026-03-26 08:56:04 -0300

    Update ex017.py

--- exercicios/ex017.py
+++ exercicios/ex017.py
@@ -1,5 +1,1 @@
-import math
-num = int(input('Digite um número: ' ))
-raiz = math.sqrt(num)
-print('a raiz de {} é igual a {}'.format(num, math.ceil(raiz)))#arredonda pra cima
-print('a raiz de {} é igual a {}'.format(num, math.floor(raiz)))
+
