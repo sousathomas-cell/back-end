01d40078f700ac6fffa9c644eb40fad734c52168
sousathomas-cell <sousa.thomas@escola.pr.gov.br>
2026-03-26 08:54:40 -0300

    Update ex018.py

--- exercicios/ex018.py
+++ exercicios/ex018.py
@@ -1,5 +1,1 @@
-from math import sqrt, floor, ceil
-num = int(input('Digite um número: ' ))
-raiz = sqrt(num)
-print('a raiz de {} é igual a {}'.format(num, ceil(raiz)))#arredonda pra cima
-print('a raiz de {} é igual a {}'.format(num, floor(raiz)))
+
