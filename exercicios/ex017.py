b70283c1e0ce0cf2f520b06c11d01856665f7ff2
sousathomas-cell <sousa.thomas@escola.pr.gov.br>
2026-04-23 11:47:32 -0300

    Update ex017.py

--- exercicios/ex017.py
+++ exercicios/ex017.py
@@ -1,1 +1,8 @@
+import math
 
+cate_oposto = float(input("digite o valor do cateto oposto:"))
+cate_adjacente = float(input("digite o valor do cateto adjacente"))
+
+hipotenusa = math.hypot(cate_oposto, cate_adjacente)
+
+print("o valor da hipotenusa vai medir{:.3f}".format(hipotenusa))
