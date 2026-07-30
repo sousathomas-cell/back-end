4c73e1e3bfa39c7a17f86d764646c11669487acd
sousathomas-cell <sousa.thomas@escola.pr.gov.br>
2026-07-30 08:46:40 -0300

    Create ex052.py

---
+++ ex052.py
@@ -0,0 +1,16 @@
+maior = 0
+menor = 0
+
+for c in range(1, 6):
+    p = float(input(f"Peso da {c} pessoa em kg: "))
+    if c == 1:
+        maior = p
+        menor = p
+    else:
+        if p > maior:
+            maior = p
+        if p < menor:
+            menor = p
+
+print(f"O maior peso lido foi {maior}kg")
+print(f"O menor peso lido foi {menor}kg")
